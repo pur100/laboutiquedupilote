f5068fdf02c4cd3fce11032b1344db30891d9b26
Hugo MANCINI <h.g.mancini@gmail.com>
2020-03-31 12:52:01 +0200

    header ok

---
+++ assets/custom.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+
+  console.log("Hello cystom")
+
+  $("#search_icon").click(function() {
+    console.log("searcg ")
+    $(".search_dropdown").toggleClass("no_show")
+  })
+
+})
